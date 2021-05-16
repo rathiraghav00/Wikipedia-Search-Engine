445fdd22f71db1fbdc77142028078f2f5e898e34
rathiraghav00 <42902117+rathiraghav00@users.noreply.github.com>
2021-05-16 11:53:32 +0530

    Adding pretty code

--- indexer2.py
+++ indexer2.py
@@ -6,7 +6,11 @@ from heapq import heappush as push, heappop as pop
 from collections import *
 import os
 
+<<<<<<< Updated upstream
 tags = ['title', 'category', 'infobox', 'text']
+=======
+tags = ["title", "category", "infobox", "text"]
+>>>>>>> Stashed changes
 
 # doc_no is not equal to pages!!!
 
@@ -15,26 +19,43 @@ total_pages = 360986
 
 # probably same as doc no
 
+<<<<<<< Updated upstream
 def writeit(tag, data):
     offset_file_path = 'offsets/' + str(tag) + '.txt'
     offsets_file = open(offset_file_path, 'a')
     score_file_path = 'score/' + str(tag) + '.txt'
     score_file = open(score_file_path, 'a')
+=======
+
+def writeit(tag, data):
+    offset_file_path = "offsets/" + str(tag) + ".txt"
+    offsets_file = open(offset_file_path, "a")
+    score_file_path = "score/" + str(tag) + ".txt"
+    score_file = open(score_file_path, "a")
+>>>>>>> Stashed changes
     offset = 0
     for key in sorted(data):
         values = data[key]
         idf = 0
         if len(values) > 0:
             idf = math.log10(float(total_pages) / float(len(values)))
+<<<<<<< Updated upstream
         curr = str(key) + ' '
         for x in values:
 
             s = x.split(':')
+=======
+        curr = str(key) + " "
+        for x in values:
+
+            s = x.split(":")
+>>>>>>> Stashed changes
             doc = s[0]
             cnt = s[1]
 
             # Check why we add a +1
             # Probably wrong
+<<<<<<< Updated upstream
 
             tfidf = (1 + math.log10(float(cnt))) * idf
 
@@ -46,16 +67,32 @@ def writeit(tag, data):
 
         # 2 for ":" and "\n"
 
+=======
+            tfidf = (1 + math.log10(float(cnt))) * idf
+
+            tfidf = "%.2f" % tfidf
+
+            curr += str(doc) + ":" + str(tfidf) + " "
+
+        offsets_file.write(str(key) + ":" + str(offset) + "\n")
+
+        # 2 for ":" and "\n"
+>>>>>>> Stashed changes
         offset += 2 + len(curr)
 
         score_file.write(str(curr))
-        score_file.write('\n')
+        score_file.write("\n")
+
 
 
 for tag in tags:
     all_files = {}
     first_lines = {}
+<<<<<<< Updated upstream
     path = 'final/' + str(tag)
+=======
+    path = "final/" + str(tag)
+>>>>>>> Stashed changes
 
     all_words = defaultdict(list)
     heap = []
@@ -64,11 +101,17 @@ for tag in tags:
     flag = [0] * len(filenames)
     i = 0
     for file in filenames:
+<<<<<<< Updated upstream
         all_files[i] = open(str(path) + '/' + str(file), 'r')
         first_lines[i] = all_files[i].readline().split('  ')
 
         # print(first_lines[i])
 
+=======
+        all_files[i] = open(str(path) + "/" + str(file), "r")
+        first_lines[i] = all_files[i].readline().split("  ")
+        # print(first_lines[i])
+>>>>>>> Stashed changes
         flag[i] = 1
         if first_lines[i][0] not in heap:
             push(heap, first_lines[i][0])
@@ -79,7 +122,10 @@ for tag in tags:
     no = 0
 
     # Follows sorted fashion of lexographical words
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     while any(flag) == 1:
         top = pop(heap)
         cur += 1
@@ -88,9 +134,17 @@ for tag in tags:
                 if first_lines[i][0] == top:
 
                     # Append in data[top]
+<<<<<<< Updated upstream
 
                     data[top].extend((first_lines[i])[1:])
                     first_lines[i] = all_files[i].readline().split('  ')
+=======
+                    data[top].extend(first_lines[i][1:])
+                    first_lines[i] = all_files[i].readline().split("  ")
+
+                    if first_lines[i][0] == "":
+                        flag[i] = 0
+>>>>>>> Stashed changes
 
                     if first_lines[i][0] == '':
                         flag[i] = 0
@@ -98,6 +152,7 @@ for tag in tags:
 
                         if first_lines[i][0] not in heap:
                             push(heap, first_lines[i][0])
+<<<<<<< Updated upstream
 
         if cur >= 50000:
 
@@ -105,13 +160,24 @@ for tag in tags:
 
             writeit(tag, data)
             print (tag, no)
+=======
+
+        if cur >= 50000:
+            # print(data)
+            writeit(tag, data)
+            print(tag, no)
+>>>>>>> Stashed changes
             data = defaultdict(list)
             no += 1
             cur = 0
 
     if cur > 0:
         writeit(tag, data)
+<<<<<<< Updated upstream
         print (tag, no)
+=======
+        print(tag, no)
+>>>>>>> Stashed changes
         data = defaultdict(list)
         no += 1
         cur = 0
--- query.py
+++ query.py
@@ -1,6 +1,9 @@
+<<<<<<< Updated upstream
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+=======
+>>>>>>> Stashed changes
 import re
 from collections import *
 import sys
@@ -8,18 +11,24 @@ from nltk.stem.snowball import SnowballStemmer
 import os
 import time
 
+<<<<<<< Updated upstream
 tags = {
     1: 'title',
     2: 'text',
     3: 'category',
     4: 'infobox',
     }
+=======
+tags = {1: "title", 2: "text", 3: "category", 4: "infobox"}
+
+>>>>>>> Stashed changes
 
 dic = defaultdict(float)
 
 # of the format this [something, tfidf score]
 # [('187', 3.29), ('204', 3.29), ('387', 3.29)]
 
+<<<<<<< Updated upstream
 offsets = [open('offsets/title.txt', 'r', encoding='ISO-8859-1'),
            open('offsets/text.txt', 'r', encoding='ISO-8859-1'),
            open('offsets/category.txt', 'r', encoding='ISO-8859-1'),
@@ -31,6 +40,23 @@ scores = [open('score/title.txt', 'r', encoding='ISO-8859-1'),
           open('score/infobox.txt', 'r', encoding='ISO-8859-1')]
 
 title_file = open('doc_title.txt', 'r', encoding='ISO-8859-1')
+=======
+offsets = [
+    open("offsets/title.txt", "r", encoding="ISO-8859-1"),
+    open("offsets/text.txt", "r", encoding="ISO-8859-1"),
+    open("offsets/category.txt", "r", encoding="ISO-8859-1"),
+    open("offsets/infobox.txt", "r", encoding="ISO-8859-1"),
+]
+
+scores = [
+    open("score/title.txt", "r", encoding="ISO-8859-1"),
+    open("score/text.txt", "r", encoding="ISO-8859-1"),
+    open("score/category.txt", "r", encoding="ISO-8859-1"),
+    open("score/infobox.txt", "r", encoding="ISO-8859-1"),
+]
+
+title_file = open("doc_title.txt", "r", encoding="ISO-8859-1")
+>>>>>>> Stashed changes
 doc_offset = []
 lines = title_file.readlines()
 cur = 0
@@ -44,11 +70,18 @@ for i in range(4):
 
     lines = offsets[i].readlines()
     for line in lines:
+<<<<<<< Updated upstream
         temp = line.split(':')
         word = ''.join(temp[0:-1])
 
         # No pair of {doc_no, id} doesnot exist
 
+=======
+        temp = line.split(":")
+        word = "".join(temp[0:-1])
+
+        # No pair of {doc_no, id} doesnot exist
+>>>>>>> Stashed changes
         if len(temp[-1]) == 0:
             continue
 
@@ -57,14 +90,19 @@ for i in range(4):
         page = int(temp[-1].strip())
         word_pos[i][word] = page
 
+<<<<<<< Updated upstream
 
  #   offsets[i].close()
+=======
+#   offsets[i].close()
+>>>>>>> Stashed changes
 
 # Regex to remove special characters
 
 def remove_special_characters(text):
 
     # Define the pattern to keep
+<<<<<<< Updated upstream
 
     pat = r'[^a-zA-z0-9.,!?/:;\"\'\s]'
     return re.sub(pat, ' ', text)
@@ -75,6 +113,17 @@ stemmer = SnowballStemmer(language='english')
 
 def tagq(words, typ):
 
+=======
+    pat = r"[^a-zA-z0-9.,!?/:;\"\'\s]"
+    return re.sub(pat, " ", text)
+
+
+stemmer = SnowballStemmer(language="english")
+
+
+def tagq(words, typ):
+
+>>>>>>> Stashed changes
     if typ == 4:
         for i in range(4):
             tagq(words, i)
@@ -82,6 +131,7 @@ def tagq(words, typ):
 
     for word in words:
         word = stemmer.stem(word.lower())
+<<<<<<< Updated upstream
 
         # word = remove_special_characters(word)
 
@@ -90,14 +140,20 @@ def tagq(words, typ):
            # word = stemmer.stem(word.lower())
             # word = remove_special_characters(word)
 
+=======
+        # word = remove_special_characters(word)
+        if word in word_pos[typ]:
+            # word = stemmer.stem(word.lower())
+            # word = remove_special_characters(word)
+>>>>>>> Stashed changes
             page = word_pos[typ][word]
             scores[typ].seek(page)
             line = scores[typ].readline()
-            line = line.split(' ')
+            line = line.split(" ")
             for w in line[1:]:
                 if len(w) <= 1:
                     continue
-                tmp = w.split(':')
+                tmp = w.split(":")
                 pg = tmp[0]
                 tfidf = float(tmp[1])
                 if pg not in dic:
@@ -108,11 +164,17 @@ def tagq(words, typ):
 def process():
 
     # Sort in decreasing order by tf-idf score
+<<<<<<< Updated upstream
 
     results = sorted(dic.items(), key=lambda item: item[1],
                      reverse=True)
 
     results = results[:min(10, len(results))]
+=======
+    results = sorted(dic.items(), key=lambda item: item[1], reverse=True)
+
+    results = results[: min(10, len(results))]
+>>>>>>> Stashed changes
 
     # print(results)
 
@@ -121,12 +183,18 @@ def process():
         pos = doc_offset[int(cur)]
         title_file.seek(pos)
         title = title_file.readline().strip()
+<<<<<<< Updated upstream
         title = title.replace(' ', '_')
         print 'https://en.wikipedia.org/wiki/' + title
+=======
+        title = title.replace(" ", "_")
+        print("https://en.wikipedia.org/wiki/" + title)
+>>>>>>> Stashed changes
 
 
 def query():
 
+<<<<<<< Updated upstream
     print 'Any Queries?  (Yes or No) \n'
 
     q = input()
@@ -146,8 +214,29 @@ def query():
 
         print 'Type Your Query:\n'
         que = input().strip('\n').lower()
+=======
+    print("Any Queries?  (Yes or No) \n")
 
-        words = que.split(' ')
+    q = input()
+
+    if q == "yes" or q == "y" or q == "Yes" or q == "Y":
+
+        clock_start = time.time()
+
+        print("Select Query Type:")
+        print("1 - Search By Title")
+        print("2 - Search By Text")
+        print("3 - Search By Category")
+        print("4 - Search By Infobox")
+        print("5 - Widesearch")
+
+        typ = int(input())
+
+        print("Type Your Query:\n")
+        que = input().strip("\n").lower()
+>>>>>>> Stashed changes
+
+        words = que.split(" ")
 
         # Clear the dictionary
 
@@ -160,14 +249,23 @@ def query():
         process()
 
         # Print the time taken
+<<<<<<< Updated upstream
 
         print 'Query time = ' + str(time.time() - clock_start)
+=======
+        print("Query time = " + str(time.time() - clock_start))
+>>>>>>> Stashed changes
 
         # Recursive Call
 
         query()
+<<<<<<< Updated upstream
     else:
 
+=======
+
+    else:
+>>>>>>> Stashed changes
         return
 
 
