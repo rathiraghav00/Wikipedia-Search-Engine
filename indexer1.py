e545797e3025bbdde2164ba1440611252b660d31
rathiraghav00 <42902117+rathiraghav00@users.noreply.github.com>
2021-05-16 11:56:47 +0530

    Beautified files

--- indexer1.py
+++ indexer1.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1brLTLdmD6oYKEdttJy7y1c-lNaFaGQNK
 """
 
-#indexer for XML data
+# indexer for XML data
 from __future__ import print_function
 import xml.etree.ElementTree as ET
 import re
@@ -22,189 +22,204 @@ from platform import python_version
 
 print(python_version())
 
-XML_file_names = os.listdir('current')
+XML_file_names = os.listdir("current")
 print(XML_file_names)
 
 # RE to remove urls
-regExp1 = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',re.DOTALL)
+regExp1 = re.compile(
+    r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+    re.DOTALL,
+)
 
 # RE to remove tags & css
-regExp2 = re.compile(r'{\|(.*?)\|}',re.DOTALL)
+regExp2 = re.compile(r"{\|(.*?)\|}", re.DOTALL)
 
 # Regular Expression to remove {{cite **}} or {{vcite **}}
-regExp3 = re.compile(r'{{v?cite(.*?)}}',re.DOTALL)
+regExp3 = re.compile(r"{{v?cite(.*?)}}", re.DOTALL)
 
 # Regular Expression to remove [[file:]]
-regExp4 = re.compile(r'\[\[file:(.*?)\]\]',re.DOTALL)
+regExp4 = re.compile(r"\[\[file:(.*?)\]\]", re.DOTALL)
+
 
 def remove_special_characters(text):
     # define the pattern to keep
-    pat = r'[^a-zA-z0-9.,!?/:;\"\'\s]' 
-    return re.sub(pat, ' ', text)
+    pat = r"[^a-zA-z0-9.,!?/:;\"\'\s]"
+    return re.sub(pat, " ", text)
+
 
 page_cnt = 0
 
 text_dic = defaultdict(list)
-text_cnt_dic ={}
+text_cnt_dic = {}
 
-category_dic =defaultdict(list)
-category_cnt_dic ={}
+category_dic = defaultdict(list)
+category_cnt_dic = {}
 
-infobox_dic =defaultdict(list)
-infobox_cnt_dic ={}
+infobox_dic = defaultdict(list)
+infobox_cnt_dic = {}
 
 title_dic = defaultdict(list)
-title_cnt_dic ={}
+title_cnt_dic = {}
 
-link_dic =defaultdict(list)
+link_dic = defaultdict(list)
 link_cnt_dic = {}
 
 # To store all the words present in the corpus
 words_dic = {}
 
-tags = {'title': 0, 'text': 1, 'category': 2,'infobox':3}
+tags = {"title": 0, "text": 1, "category": 2, "infobox": 3}
+
 
 def clean():
-    text_cnt_dic ={}
-    category_cnt_dic ={}
-    infobox_cnt_dic ={}
-    title_cnt_dic ={}
+    text_cnt_dic = {}
+    category_cnt_dic = {}
+    infobox_cnt_dic = {}
+    title_cnt_dic = {}
     link_cnt_dic = {}
     words_dic = {}
 
+
 def clean2():
     text_dic = defaultdict(list)
-    category_dic =defaultdict(list)
-    infobox_dic =defaultdict(list)
+    category_dic = defaultdict(list)
+    infobox_dic = defaultdict(list)
     title_dic = defaultdict(list)
     link_dic = defaultdict(list)
 
+
 stopwords = {}
-stemmer = SnowballStemmer(language = 'english',ignore_stopwords=True)
+stemmer = SnowballStemmer(language="english", ignore_stopwords=True)
 
 # Stemming stop words
-with open('data/stopwords.txt','r') as file:
-    words = file.read().split('\n')
-    for word in  words:
+with open("data/stopwords.txt", "r") as file:
+    words = file.read().split("\n")
+    for word in words:
         word = stemmer.stem(word.lower())
-        stopwords[word]=1
+        stopwords[word] = 1
 
 # Update title_cnt_dic and text_dic_cnt
 
-def update(s,tag):
+
+def update(s, tag):
     if s:
-        words = re.split('[^A-Za-z0-9]', s)
+        words = re.split("[^A-Za-z0-9]", s)
         for word in words:
             word = stemmer.stem(word.lower())
-            if len(word)>=2:
-                words_dic[word]=1
-                if(tag=='title'):
+            if len(word) >= 2:
+                words_dic[word] = 1
+                if tag == "title":
                     if word in title_cnt_dic:
-                        title_cnt_dic[word]+=1
-                    else :
-                        title_cnt_dic[word]=1
-                elif tag=='text':
+                        title_cnt_dic[word] += 1
+                    else:
+                        title_cnt_dic[word] = 1
+                elif tag == "text":
                     if word in infobox_cnt_dic:
-                        text_cnt_dic[word]+=1
-                    else :
-                        text_cnt_dic[word]=1
+                        text_cnt_dic[word] += 1
+                    else:
+                        text_cnt_dic[word] = 1
 
-#done
+
+# done
 def category_updater(text):
     rex = re.findall("\[\[Category:(.*?)\]\]", str(text))
     if rex:
-      #  print(rex)
+        #  print(rex)
         for words in rex:
-          #  print(words)
-            #words = words.split(' ')
+            #  print(words)
+            # words = words.split(' ')
             pattern = re.compile("[^a-zA-Z0-9]")
             words = re.split(pattern, words)
             for word in words:
-              #  print(word)
+                #  print(word)
                 word = stemmer.stem(word.lower())
-                if len(word)>1:
-                    words_dic[word]=1 
+                if len(word) > 1:
+                    words_dic[word] = 1
                     if word in category_cnt_dic:
-                        category_cnt_dic[word]+=1
-                    else :
-                        category_cnt_dic[word]=1
+                        category_cnt_dic[word] += 1
+                    else:
+                        category_cnt_dic[word] = 1
 
-#done
+
+# done
 def infobox_updater(s):
-    tempword = re.findall("{{Infobox((.|\n)*?)}}", str(s)) # get all data between infobox{{ ----- }}
-    if tempword :
-        for temp in tempword :
-            for word in temp : 
+    tempword = re.findall(
+        "{{Infobox((.|\n)*?)}}", str(s)
+    )  # get all data between infobox{{ ----- }}
+    if tempword:
+        for temp in tempword:
+            for word in temp:
                 pattern = re.compile("[^a-zA-Z0-9]")
                 temp = re.split(pattern, word)
-                for t in temp :
+                for t in temp:
                     t = stemmer.stem(t.lower())
-                    if t :
-                        if len(t) <= 2 :
+                    if t:
+                        if len(t) <= 2:
                             continue
-                        if  t not in stopwords :
-                            words_dic[t]=1
-                            if t not in infobox_cnt_dic :
+                        if t not in stopwords:
+                            words_dic[t] = 1
+                            if t not in infobox_cnt_dic:
                                 infobox_cnt_dic[t] = 1
-                            else :
+                            else:
                                 infobox_cnt_dic[t] += 1
 
-doc_no =0
+
+doc_no = 0
 doc_cnt = 0
 
-doc_titles = open('doc_title.txt','a',encoding= 'utf-8')
+doc_titles = open("doc_title.txt", "a", encoding="utf-8")
 
-file_no =1
+file_no = 1
 for i in range(len(XML_file_names)):
-    for event,element in ET.iterparse('current/'+XML_file_names[i],events=("start","end")):
+    for event, element in ET.iterparse(
+        "current/" + XML_file_names[i], events=("start", "end")
+    ):
 
         tag = element.tag
-        #print(tag)
+        # print(tag)
 
         # Check this out!!????
-        #it is a namespace actually, and we use namespaces because for eg if we have a field ID both in
-	#as well as teacher class then we use a namespace like a link in XML. But we need the end portion after that namespace
-        tag = tag[tag.rfind('}')+1:] #namespace
+        # it is a namespace actually, and we use namespaces because for eg if we have a field ID both in
+        # as well as teacher class then we use a namespace like a link in XML. But we need the end portion after that namespace
+        tag = tag[tag.rfind("}") + 1 :]  # namespace
 
+        #         original {http://www.mediawiki.org/xml/export-0.10/}mediawiki
+        # after r.find mediawiki
+        # original {http://www.mediawiki.org/xml/export-0.10/}siteinfo
+        # after r.find siteinfo
+        # original {http://www.mediawiki.org/xml/export-0.10/}sitename
+        # after r.find sitename
+        # original {http://www.mediawiki.org/xml/export-0.10/}sitename
+        # after r.find sitename
+        # original {http://www.mediawiki.org/xml/export-0.10/}dbname
+        # after r.find dbname
+        # original {http://www.mediawiki.org/xml/export-0.10/}dbname
 
-#         original {http://www.mediawiki.org/xml/export-0.10/}mediawiki
-# after r.find mediawiki
-# original {http://www.mediawiki.org/xml/export-0.10/}siteinfo
-# after r.find siteinfo
-# original {http://www.mediawiki.org/xml/export-0.10/}sitename
-# after r.find sitename
-# original {http://www.mediawiki.org/xml/export-0.10/}sitename
-# after r.find sitename
-# original {http://www.mediawiki.org/xml/export-0.10/}dbname
-# after r.find dbname
-# original {http://www.mediawiki.org/xml/export-0.10/}dbname
-
-        if tag == 'page' and event =='end':  #one page at a time
+        if tag == "page" and event == "end":  # one page at a time
 
             for w in words_dic:
-                #w = remove_special_characters(w)
+                # w = remove_special_characters(w)
                 for t in tags:
-                    if t=='title':
-                        if w in title_cnt_dic :
+                    if t == "title":
+                        if w in title_cnt_dic:
 
                             # $(tags)_cnt_dic has already beeen calculated
-                            s = " "+ str(doc_no)+":" + str(title_cnt_dic[w])
+                            s = " " + str(doc_no) + ":" + str(title_cnt_dic[w])
                             title_dic[w].append(s)
-                    elif t=='text':
+                    elif t == "text":
                         if w in text_cnt_dic:
-                            s = " "+ str(doc_no)+":" + str(text_cnt_dic[w])
+                            s = " " + str(doc_no) + ":" + str(text_cnt_dic[w])
                             text_dic[w].append(s)
-                    elif t=='category':
+                    elif t == "category":
                         if w in category_cnt_dic:
-                            s = " "+ str(doc_no)+":" + str(category_cnt_dic[w])
+                            s = " " + str(doc_no) + ":" + str(category_cnt_dic[w])
                             category_dic[w].append(s)
-                    elif t=='infobox':
+                    elif t == "infobox":
                         if w in infobox_cnt_dic:
-                            s = " "+ str(doc_no)+":" + str(infobox_cnt_dic[w])
+                            s = " " + str(doc_no) + ":" + str(infobox_cnt_dic[w])
                             infobox_dic[w].append(s)
 
-            doc_no+=1
-            doc_cnt+=1
+            doc_no += 1
+            doc_cnt += 1
 
             element.clear()
             title_cnt_dic.clear()
@@ -213,157 +228,153 @@ for i in range(len(XML_file_names)):
             infobox_cnt_dic.clear()
             words_dic.clear()
 
-        elif tag =='title' and event =='end':
+        elif tag == "title" and event == "end":
             text = element.text
             text.strip()
-            text.replace(" ","_")
+            text.replace(" ", "_")
             text = re.sub("\s+", "_", text.strip())
-            doc_titles.write(str(text)+'\n')
-            update(str(text),tag)
-        elif tag =='text' and event =='end':
-                text = str(element.text)
-                text = regExp1.sub('',text)
-                text = regExp2.sub('',text)
-                text = regExp3.sub('',text)
-                text = regExp4.sub('',text)
-                update(str(text),tag)
-                category_updater(str(text))
-                infobox_updater(str(text))
+            doc_titles.write(str(text) + "\n")
+            update(str(text), tag)
+        elif tag == "text" and event == "end":
+            text = str(element.text)
+            text = regExp1.sub("", text)
+            text = regExp2.sub("", text)
+            text = regExp3.sub("", text)
+            text = regExp4.sub("", text)
+            update(str(text), tag)
+            category_updater(str(text))
+            infobox_updater(str(text))
 
-        if doc_cnt>=20000:
-            doc_cnt=0
-            title_path = 'final/title/' + str(file_no) +'.txt'
-            
-            file = open(title_path,'w',encoding='utf-8')
-            s= ""
+        if doc_cnt >= 20000:
+            doc_cnt = 0
+            title_path = "final/title/" + str(file_no) + ".txt"
+
+            file = open(title_path, "w", encoding="utf-8")
+            s = ""
             for word in sorted(title_dic):
-                s= word + ' '
+                s = word + " "
                 for vl in title_dic[word]:
-                    s+= vl +' '
-                s+= '\n'
+                    s += vl + " "
+                s += "\n"
                 file.write(s)
             file.close()
             title_dic.clear()
-            
-            
-            
-            
-            print('title',file_no)
 
-            text_path = 'final/text/' + str(file_no) +'.txt'
-            file = open(text_path,'w',encoding='utf-8')
-            s= ""
+            print("title", file_no)
+
+            text_path = "final/text/" + str(file_no) + ".txt"
+            file = open(text_path, "w", encoding="utf-8")
+            s = ""
             for word in sorted(text_dic):
-                s= word + ' '
+                s = word + " "
                 for vl in text_dic[word]:
-                    s+= vl +' '
-                s+= '\n'
+                    s += vl + " "
+                s += "\n"
                 file.write(s)
             file.close()
-            
+
             text_dic.clear()
-            
-            print('text',file_no)
 
-            cat_path = 'final/category/' + str(file_no) +'.txt'
-            file = open(cat_path,'w',encoding='utf-8')
-            s= ""
+            print("text", file_no)
+
+            cat_path = "final/category/" + str(file_no) + ".txt"
+            file = open(cat_path, "w", encoding="utf-8")
+            s = ""
             for word in sorted(category_dic):
-                s= word + ' '
+                s = word + " "
                 for vl in category_dic[word]:
-                    s+= vl +' '
-                s+= '\n'
+                    s += vl + " "
+                s += "\n"
                 file.write(s)
             file.close()
-            
-            category_dic.clear()
-            
-            
-            print('cat',file_no)
 
-            info_path = 'final/infobox/' + str(file_no) +'.txt'
-            file = open(info_path,'w',encoding='utf-8')
-            s= ""
+            category_dic.clear()
+
+            print("cat", file_no)
+
+            info_path = "final/infobox/" + str(file_no) + ".txt"
+            file = open(info_path, "w", encoding="utf-8")
+            s = ""
             for word in sorted(infobox_dic):
-                s= word + ' '
+                s = word + " "
                 for vl in infobox_dic[word]:
-                    s+= vl +' '
-                s+= '\n'
+                    s += vl + " "
+                s += "\n"
                 file.write(s)
             file.close()
-            
+
             infobox_dic.clear()
 
-            print('info',file_no)
-            file_no+=1
-            
+            print("info", file_no)
+            file_no += 1
 
-#since the number of documents may not be a multiple of 20000 so I need to write one more if statement like above
-if doc_cnt>0:
+
+# since the number of documents may not be a multiple of 20000 so I need to write one more if statement like above
+if doc_cnt > 0:
     print(doc_cnt)
-    doc_cnt=0
-    #file_no = doc_no%5000 + 1
-    
-    title_path = 'final/title/' + str(file_no) +'.txt'
-    
-    file = open(title_path,'w',encoding='utf-8')
-    s= ""
+    doc_cnt = 0
+    # file_no = doc_no%5000 + 1
+
+    title_path = "final/title/" + str(file_no) + ".txt"
+
+    file = open(title_path, "w", encoding="utf-8")
+    s = ""
     for word in sorted(title_dic):
-        s= word + ' '
+        s = word + " "
         for vl in title_dic[word]:
-            s+= vl +' '
-        s+= '\n'
+            s += vl + " "
+        s += "\n"
         file.write(s)
     file.close()
-    print('title',file_no)
-    
+    print("title", file_no)
+
     title_dic.clear()
 
-    text_path = 'final/text/' + str(file_no) +'.txt'
-    file = open(text_path,'w',encoding='utf-8')
-    s= ""
+    text_path = "final/text/" + str(file_no) + ".txt"
+    file = open(text_path, "w", encoding="utf-8")
+    s = ""
     for word in sorted(text_dic):
-        s= word + ' '
+        s = word + " "
         for vl in text_dic[word]:
-            s+= vl +' '
-        s+= '\n'
+            s += vl + " "
+        s += "\n"
         file.write(s)
     file.close()
-    
+
     text_dic.clear()
 
-    print('text',file_no)
+    print("text", file_no)
 
-    cat_path = 'final/category/' + str(file_no) +'.txt'
-    file = open(cat_path,'w',encoding='utf-8')
-    s= ""
+    cat_path = "final/category/" + str(file_no) + ".txt"
+    file = open(cat_path, "w", encoding="utf-8")
+    s = ""
     for word in sorted(category_dic):
-        s= word + ' '
+        s = word + " "
         for vl in category_dic[word]:
-            s+= vl +' '
-        s+= '\n'
+            s += vl + " "
+        s += "\n"
         file.write(s)
     file.close()
-    
+
     category_dic.clear()
 
-    print('cat',file_no)
+    print("cat", file_no)
 
-    info_path = 'final/infobox/' + str(file_no) +'.txt'
-    file = open(info_path,'w',encoding='utf-8')
-    s= ""
+    info_path = "final/infobox/" + str(file_no) + ".txt"
+    file = open(info_path, "w", encoding="utf-8")
+    s = ""
     for word in sorted(infobox_dic):
-        s= word + ' '
+        s = word + " "
         for vl in infobox_dic[word]:
-            s+= vl +' '
-        s+= '\n'
+            s += vl + " "
+        s += "\n"
         file.write(s)
     file.close()
-    
+
     infobox_dic.clear()
 
-    print('info',file_no)
-    file_no +=1
-    clean2()        
+    print("info", file_no)
+    file_no += 1
+    clean2()
 
 print(doc_no)
--- query.py
+++ query.py
@@ -1,4 +1,3 @@
-
 import re
 from collections import *
 import sys
@@ -6,87 +5,100 @@ from nltk.stem.snowball import SnowballStemmer
 import os
 import time
 
-tags = {1:'title',2:'text', 3:'category', 4 : 'infobox'}
+tags = {1: "title", 2: "text", 3: "category", 4: "infobox"}
 
 
 dic = defaultdict(float)
 # of the format this [something, tfidf score]
 # [('187', 3.29), ('204', 3.29), ('387', 3.29)]
 
-offsets = [open('offsets/title.txt','r',encoding='ISO-8859-1'),open('offsets/text.txt','r',encoding='ISO-8859-1'),open('offsets/category.txt','r',encoding='ISO-8859-1'),open('offsets/infobox.txt','r',encoding='ISO-8859-1')]
+offsets = [
+    open("offsets/title.txt", "r", encoding="ISO-8859-1"),
+    open("offsets/text.txt", "r", encoding="ISO-8859-1"),
+    open("offsets/category.txt", "r", encoding="ISO-8859-1"),
+    open("offsets/infobox.txt", "r", encoding="ISO-8859-1"),
+]
 
-scores = [open('score/title.txt','r',encoding='ISO-8859-1'),open('score/text.txt','r',encoding='ISO-8859-1'),open('score/category.txt','r',encoding='ISO-8859-1'),open('score/infobox.txt','r',encoding='ISO-8859-1')]
+scores = [
+    open("score/title.txt", "r", encoding="ISO-8859-1"),
+    open("score/text.txt", "r", encoding="ISO-8859-1"),
+    open("score/category.txt", "r", encoding="ISO-8859-1"),
+    open("score/infobox.txt", "r", encoding="ISO-8859-1"),
+]
 
-title_file  = open('doc_title.txt','r',encoding='ISO-8859-1')
+title_file = open("doc_title.txt", "r", encoding="ISO-8859-1")
 doc_offset = []
 lines = title_file.readlines()
-cur =0
+cur = 0
 for line in lines:
     doc_offset.append(cur)
-    cur+=len(line)+1
+    cur += len(line) + 1
 
-word_pos  = [defaultdict(list) for i in range(4)]
+word_pos = [defaultdict(list) for i in range(4)]
 
 for i in range(4):
 
     lines = offsets[i].readlines()
     for line in lines:
-        temp = line.split(':')
+        temp = line.split(":")
         word = "".join(temp[0:-1])
-        
+
         # No pair of {doc_no, id} doesnot exist
-        if len(temp[-1])==0:
+        if len(temp[-1]) == 0:
             continue
 
         # page is location of word in offset/{$tag}
         page = int(temp[-1].strip())
         word_pos[i][word] = page
 
- #   offsets[i].close()
+#   offsets[i].close()
 
 # Regex to remove special characters
 def remove_special_characters(text):
 
     # Define the pattern to keep
-    pat = r'[^a-zA-z0-9.,!?/:;\"\'\s]' 
-    return re.sub(pat, ' ', text)
+    pat = r"[^a-zA-z0-9.,!?/:;\"\'\s]"
+    return re.sub(pat, " ", text)
 
-stemmer = SnowballStemmer(language = 'english')
 
-def tagq(words,typ):
-    
-    if typ==4:
+stemmer = SnowballStemmer(language="english")
+
+
+def tagq(words, typ):
+
+    if typ == 4:
         for i in range(4):
-            tagq(words,i)
+            tagq(words, i)
         return
 
-    for word in words :
+    for word in words:
         word = stemmer.stem(word.lower())
-        #word = remove_special_characters(word)
+        # word = remove_special_characters(word)
         if word in word_pos[typ]:
-           # word = stemmer.stem(word.lower())
-            #word = remove_special_characters(word)
+            # word = stemmer.stem(word.lower())
+            # word = remove_special_characters(word)
             page = word_pos[typ][word]
             scores[typ].seek(page)
             line = scores[typ].readline()
-            line = line.split(' ')
+            line = line.split(" ")
             for w in line[1:]:
-                if len(w)<=1:
+                if len(w) <= 1:
                     continue
-                tmp = w.split(':')
+                tmp = w.split(":")
                 pg = tmp[0]
-                tfidf =float(tmp[1])
+                tfidf = float(tmp[1])
                 if pg not in dic:
-                    dic[pg]=0
-                dic[pg]+=tfidf
+                    dic[pg] = 0
+                dic[pg] += tfidf
+
 
 def process():
 
-    #Sort in decreasing order by tf-idf score
-    results = sorted(dic.items(),key=lambda item: item[1],reverse=True)
+    # Sort in decreasing order by tf-idf score
+    results = sorted(dic.items(), key=lambda item: item[1], reverse=True)
 
-    results = results[:min(10,len(results))]
-    
+    results = results[: min(10, len(results))]
+
     # print(results)
 
     for cur in results:
@@ -94,48 +106,50 @@ def process():
         pos = doc_offset[int(cur)]
         title_file.seek(pos)
         title = title_file.readline().strip()
-        title = title.replace(' ','_')
-        print ("https://en.wikipedia.org/wiki/" + title)
+        title = title.replace(" ", "_")
+        print("https://en.wikipedia.org/wiki/" + title)
+
 
 def query():
 
-    print('Any Queries?  (Yes or No) \n')
+    print("Any Queries?  (Yes or No) \n")
 
     q = input()
 
-    if q=='yes' or q =='y' or q == 'Yes' or q == 'Y':
+    if q == "yes" or q == "y" or q == "Yes" or q == "Y":
 
         clock_start = time.time()
 
-        print('Select Query Type:')
-        print('1 - Search By Title')
-        print('2 - Search By Text')
-        print('3 - Search By Category')
-        print('4 - Search By Infobox')
-        print('5 - Widesearch')
+        print("Select Query Type:")
+        print("1 - Search By Title")
+        print("2 - Search By Text")
+        print("3 - Search By Category")
+        print("4 - Search By Infobox")
+        print("5 - Widesearch")
 
         typ = int(input())
 
-        print('Type Your Query:\n')
-        que = input().strip('\n').lower()
+        print("Type Your Query:\n")
+        que = input().strip("\n").lower()
 
-        words = que.split(' ')
+        words = que.split(" ")
 
         # Clear the dictionary
         dic.clear()
-        
-        tagq(words,typ-1)
+
+        tagq(words, typ - 1)
 
         # Processing
         process()
 
         # Print the time taken
-        print ("Query time = " +str(time.time()-clock_start))
+        print("Query time = " + str(time.time() - clock_start))
 
         # Recursive Call
         query()
-        
-    else :
+
+    else:
         return
+
 
 query()
